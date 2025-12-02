3d17a1992d76562e157c44a6359ede61915fe7bf
koochanghyen <kuku0746@naver.com>
2025-12-03 01:06:53 +0900

    코딩 기초 트레이닝 7일차 1,2

---
+++ src/main/java/day7_1.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class day7_1 {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 4, 3};
+        int[][] queries =
+                {{0,4,1},
+                {0,3,2},
+                {0,3,3}};
+
+        int a;
+        int b;
+        int c;
+
+        for (int i = 0; i < queries.length; i++) {
+            a = queries[i][0];
+            b = queries[i][1];
+            c = queries[i][2];
+
+            for (int j = a; j<=b; j++){
+                if (j%c==0){
+                    arr[j] = arr[j]+1;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+}
+
+//쿼리s에 0,1인덱스를 변수로 만들어서
+// 각각 arr의 범위를 지정해주고
+// 그 범위 내에 쿼리에 2번째 값이랑 배수인게 있으면
+// 그 배수를 +1
---
+++ src/main/java/day7_2.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class day7_2 {
+    public static void main(String[] args) {
+    int l = 5;
+    int r = 555;
+
+    ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = l; i <= r; i++) {
+            String s = String.valueOf(i);
+            boolean ok = true;
+
+            for(char c : s.toCharArray()){
+                if (c!='0' && c!='5'){
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok){
+                list.add(i);
+            }
+        }
+
+        if (list.isEmpty()){
+            list.add(-1);
+        }
+
+        Integer[] integers = list.stream().toArray(Integer[]::new);
+
+        int[] arr = Arrays.stream(integers).mapToInt(Integer::intValue).toArray();
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+
+
+
+
+
+
+        //정수 l과 r이 주어졌을 때, l 이상 r이하의 정수 중에서
+        // 숫자 "0"과 "5"로만 이루어진 모든 정수를 *오름차순으로 저장한 배열*을 //리스트 이용해서 값들을 받고 배열로 변환
+        // return 하는 solution 함수를 완성해 주세요.
+        //만약 그러한 정수가 없다면, -1이 담긴 배열을 return 합니다.
+    }
+}
