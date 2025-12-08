a6bd6bd1fc26cc64bab05f4b01fbb4faffa3e0ac
koochanghyen <kuku0746@naver.com>
2025-12-09 00:34:27 +0900

    day8 마무리

---
+++ src/main/java/Coding_basic_training/Day8_3.java
@@ -0,0 +1,29 @@
+package Coding_basic_training;
+
+public class Day8_3 {
+    public static void main(String[] args) {
+        String number = "78720646226947352489";
+        int answer = 0;
+
+
+        int[] arr = new int[number.length()];
+
+        for (int i = 0; i < number.length(); i++) {
+            arr[i] = number.charAt(i) - '0';
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            answer = answer+arr[i];
+        }
+
+        answer = answer%9;
+
+        System.out.println(answer);
+
+
+
+
+        //음이 아닌 정수를 9로 나눈 나머지는 그 정수의 각 자리 숫자의 합을 9로 나눈 나머지와 같은 것이 알려져 있습니다.
+        //이 사실을 이용하여 음이 아닌 정수가 문자열 number로 주어질 때, 이 정수를 9로 나눈 나머지를 return 하는 solution 함수를 작성해주세요.
+    }
+}
---
+++ src/main/java/Coding_basic_training/Day8_4.java
@@ -0,0 +1,40 @@
+package Coding_basic_training;
+
+public class Day8_4 {
+    public static void main(String[] args) {
+
+        String my_string = "rermgorpsam";
+        int[][] queries = {
+                {2,3},
+                {0,7},
+                {5,9},
+                {6,10}
+        };
+
+        String result = "";
+        String reverse = "";
+        int a=0;
+        int b=0;
+
+        for (int i = 0; i < queries.length; i++) {
+            for (int j = 0; j < 1; j++) {
+                a = queries[i][j];
+                b = queries[i][j+1];
+
+                result = my_string.substring(a,b+1); // 일반 문자열
+
+                StringBuffer sb = new StringBuffer(result); //스트링 버퍼생성
+                reverse = sb.reverse().toString(); //뒤집은 문자열
+
+                StringBuilder full = new StringBuilder(my_string);
+                full.replace(a, b + 1, reverse);
+                my_string = full.toString();
+            }
+        }
+        System.out.println(my_string);
+
+    }
+}
+/*
+    솔직히 이번 문제가 좀어려워서 구글링을 많이 하면서 풀었던거 같다.
+ */
